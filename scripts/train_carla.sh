0a9df6812bc3189cd08dbc0db3eaeb695a59ff1b
Tewodros Amberbir <tedyhabtegebrial@gmail.com>
2019-11-24 17:17:19 +0100

    ...

--- scripts/train_carla.sh
+++ scripts/train_carla.sh
@@ -1,5 +1,1 @@
-python3.6 ../train.py --name spade_on_carla_five_cities --dataset_mode custom --use_vae \
-        --label_dir [path_to_labels] --image_dir /data/teddy/temporary_carla_spade/images \
-        --label_dir /data/teddy/temporary_carla_spade/labels \
-        --gpu_ids 4,5,6,7 --label_nc 13 --no_instance --batchSize 4 \
-        --checkpoints_dir /home/habtegebrial/experiments_eccv --load_size 256 --crop_size 256
+python3.6 ../train.py --name spade_on_carla_five_cities --dataset_mode custom --use_vae --label_dir /data/teddy/temporary_carla_spade/label_path --image_dir /data/teddy/temporary_carla_spade/images --label_dir /data/teddy/temporary_carla_spade/labels --gpu_ids 4,5,6,7 --label_nc 13 --no_instance --batchSize 4 --checkpoints_dir /home/habtegebrial/experiments_eccv --load_size 256 --crop_size 256
